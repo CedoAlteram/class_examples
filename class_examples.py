2fdd65ec16634e09c04a8908728f837eaec23db6
obelisk <aaron.reyna@gmail.com>
2017-01-16 17:26:08 -0800

    Added in the addition of total area, i.e. things to do after we added in shapes.

--- class_examples.py
+++ class_examples.py
@@ -1,5 +1,7 @@
 import sys
 #An example of a class
+
+
 class Shape:
 
     def __init__(self, x, y):
@@ -16,7 +18,7 @@ class Shape:
 
     def describe(self, text):
         self.description = text
-        return text + " and this was added by a def"
+        return text + " and the value of the"
 
     def authorName(self, text):
         self.author = text
@@ -28,11 +30,24 @@ class Shape:
 
 x = int(sys.argv[1])
 y = int(sys.argv[2])
+x2 = int(sys.argv[3])
+y2 = int(sys.argv[4])
 
 Shape1 = Shape(x,y)
+Shape2 = Shape(x2,y2)
 
-print(Shape1.area())
-print(Shape1.perimeter())
-print(Shape1.scaleSize(0.5))
-print(Shape1.describe("this is the describe class"))
-print("all clear go home")
+#print(Shape1.area())
+#print(Shape2.area())
+#print(Shape1.perimeter())
+#print(Shape1.scaleSize(0.5))
+print(Shape1.describe("Aaron, this is the first shape,"), "area is", Shape1.area())
+print(Shape2.describe("Aaron, this is the second Shape,"), "area is", Shape2.area())
+#print(Shape1.description)
+#print("all clear go home")
+
+first_area = Shape1.area()
+second_area = Shape2.area()
+
+total_area = first_area + second_area
+
+print("The total area is of both shapes is", total_area)
